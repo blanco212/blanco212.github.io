c5e034270811185f4aa82c3c46ec3a4e17b5460c
blanco212 morel <hmorel974@noscihigh.org>
2023-06-08 20:35:27 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -48,7 +48,13 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-
+    createPlatform(400, 600, 200, 20);
+    createPlatform(100, 700, 200, 20);
+    createPlatform(100, 400, 200, 20); 
+    createPlatform(300, 200, 200, 20); 
+    createPlatform(900, 200, 200, 20); 
+    
+    
 
     
     // TODO 2
@@ -57,7 +63,7 @@ $(function () {
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-
+    createCollectable('diamond',30,10,)
 
 
     // TODO 3
@@ -66,7 +72,8 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
+    createCannon("right", 600, 2000);
+    createCannon("right", 200, 1000);
 
 
     /////////////////////////////////////////////////
